9f9d0ea53adf06adcab19b80d4d028e1b1659aca
Rob Callaghan <rob_cal_uk@hotmail.com>
2022-06-19 22:45:41 +0100

    move to jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
+import CssBaseline from "@mui/material/CssBaseline";
 import { useState, useEffect } from "react";
 import { supabase } from "./supabaseClient";
 import { Fragment } from "react";
@@ -7,6 +8,7 @@ import Auth from "./components/Auth";
 import FilmList from "./components/FilmList";
 import { Container } from "@mui/system";
 import Typography from "@mui/material/Typography";
+import BottomBar from "./components/BottomBar";
 
 const App = () => {
   const [session, setSession] = useState(null);
@@ -21,13 +23,17 @@ const App = () => {
 
   return (
     <Container maxWidth="sm">
-      <Typography component="h1" variant="h3">
+      <CssBaseline />
+      <Typography component="h1" variant="h3" align="center" mt={4}>
         Film Night
       </Typography>
       {!session ? (
         <Auth />
       ) : (
-        <FilmList key={session.user.id} session={session} />
+        <Fragment>
+          <FilmList key={session.user.id} session={session} />
+          <BottomBar />
+        </Fragment>
       )}
     </Container>
   );
