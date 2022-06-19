029217f94cb791ff422f97f0a90fc8b4bd78dfd8
Rob Callaghan <rob_cal_uk@hotmail.com>
2022-06-19 22:46:00 +0100

    new BottomBar component

---
+++ src/components/BottomBar.jsx
@@ -0,0 +1,26 @@
+import { supabase } from "../supabaseClient";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import LogoutIcon from "@mui/icons-material/Logout";
+import AddFilm from "./AddFilm";
+
+const BottomBar = () => {
+  return (
+    <AppBar position="fixed" color="primary" sx={{ top: "auto", bottom: 0 }}>
+      <Toolbar>
+        {/* <StyledFab color="secondary" aria-label="add">
+          <AddIcon />
+        </StyledFab> */}
+        <AddFilm />
+        <Box sx={{ flexGrow: 1 }} />
+        <IconButton color="inherit" onClick={() => supabase.auth.signOut()}>
+          <LogoutIcon />
+        </IconButton>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default BottomBar;
