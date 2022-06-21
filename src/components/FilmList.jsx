5ed993dcf44370d09ba1b1e70d993b2f217fd0e4
Rob Callaghan <rob_cal_uk@hotmail.com>
2022-06-21 21:35:46 +0100

    implementing watched / watchlist toggle functionality

--- src/components/FilmList.jsx
+++ src/components/FilmList.jsx
@@ -8,9 +8,13 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import DeleteFilm from "./DeleteFilm";
 import EditFilm from "./EditFilm";
+import ToggleButton from "@mui/material/ToggleButton";
+import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
+import Box from "@mui/material/Box";
 
 const FilmList = () => {
   const [films, setFilms] = useState([]);
+  const [watchedToggle, setWatchedToggle] = useState("watchlist");
 
   useEffect(() => {
     fetchFilms().catch(console.error);
@@ -25,27 +29,50 @@ const FilmList = () => {
     else setFilms(films);
   };
 
+  const toggleWatchedFilms = () => {
+    setWatchedToggle(watchedToggle === "watchlist" ? "watched" : "watchlist");
+  };
+
+  const filteredFilms = films.filter(
+    watchedToggle === "watched"
+      ? (film) => film.watched
+      : (film) => !film.watched
+  );
+
   return (
     <Fragment>
-      <TableContainer>
-        <Table>
+      <Box sx={{ my: 2 }} style={{ textAlign: "center" }}>
+        <ToggleButtonGroup
+          color="primary"
+          value={watchedToggle}
+          exclusive
+          onChange={toggleWatchedFilms}
+        >
+          <ToggleButton value="watchlist">Watch List</ToggleButton>
+          <ToggleButton value="watched">Watched</ToggleButton>
+        </ToggleButtonGroup>
+      </Box>
+      <TableContainer sx={{ my: 2 }}>
+        <Table size="small">
           <TableHead>
             <TableRow>
               <TableCell>Film Name</TableCell>
               <TableCell>Owner</TableCell>
-              <TableCell>Rating</TableCell>
+              {watchedToggle === "watched" && <TableCell>Rating</TableCell>}
               <TableCell></TableCell>
               <TableCell></TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-            {films.map((film, index) => (
+            {filteredFilms.map((film, index) => (
               <TableRow key={index}>
                 <TableCell component="th" scope="row">
                   {film.name}
                 </TableCell>
                 <TableCell>{film.owner}</TableCell>
-                <TableCell>{film.c_rating + film.r_rating}</TableCell>
+                {watchedToggle === "watched" && (
+                  <TableCell>{film.c_rating + film.r_rating}</TableCell>
+                )}
                 <TableCell>
                   <EditFilm
                     index={index}
